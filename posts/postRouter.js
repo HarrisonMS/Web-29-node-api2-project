5f71f11da63905d54ab1ae913012dc0cd241e7af
Harrison <hmseaborn@gmail.com>
2020-04-07 14:10:01 -0700

    get by id
    console loging get request

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -18,4 +18,12 @@ router.get(`/`, (req, res) => {
   })
 })
 
+router.get(`/:id`, (req, res) => {
+  const id = req.params.id
+  Posts.findById(id)
+  .then(post => {
+    console.log(post)
+  })
+})
+
   module.exports = router;
